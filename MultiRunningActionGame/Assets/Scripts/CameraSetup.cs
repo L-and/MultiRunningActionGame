439e97c3c848aca09bad1ddba16a49e27618cc7b
L-and <ksw1812@naver.com>
2022-04-24 19:13:02 +0900

    인게임 구현

---
+++ MultiRunningActionGame/Assets/Scripts/CameraSetup.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class CameraSetup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
