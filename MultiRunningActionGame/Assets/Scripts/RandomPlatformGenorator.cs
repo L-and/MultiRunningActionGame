bc9197633d9174d38365e79b510b898376ef3fa4
L-and <ksw1812@naver.com>
2022-05-03 00:52:48 +0900

    해상도설정 완료

--- MultiRunningActionGame/Assets/Scripts/RandomPlatformGenorator.cs
+++ MultiRunningActionGame/Assets/Scripts/RandomPlatformGenorator.cs
@@ -1,10 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class RandomPlatformGenorator : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    /// 플랫폼생성관련 변수들 ///
+    public int platformOffsetY; // 플랫폼 사이의 높이
+    
+    /// 컴포넌트 변수들 ///
+    public Tilemap tilemap;
+    public TileData platformprefab;
+
     void Start()
     {
         
